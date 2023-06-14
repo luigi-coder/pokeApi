de3cc55bbb41c9f03804f90d3299bc7e26923ad7
luigi-coder <luigicoder28@gmail.com>
2023-06-14 19:21:25 -0300

    ...

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import reactLogo from '../../assets/react.svg';
-import imgNavBar from '../../assets/navBar.svg';
+import imgNavBar from '../../assets/navbar.svg';
 import './Header.css';
 
 function Header({ handleClick }) {
